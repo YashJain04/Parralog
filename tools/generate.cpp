7634aa023496cac8a302d09dc9221db3e2c5e107
Yash Jain <yashjain200304@gmail.com>
2025-09-10 10:27:46 -0400

    improved log gen

--- tools/generate.cpp
+++ tools/generate.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
+#include <vector>
+#include <random>
 
 int main(int argc, char* argv[]) {
     std::cout << "Log Generator\n";
@@ -14,8 +17,50 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    auto now = std::chrono::system_clock::now();
+    auto ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
+    long long timestamp = ms.time_since_epoch().count();
+
+    std::vector <std::string> services;
+
+
+    for (int count { 1 }; count < 101; count++) {
+        if (count < 10) {
+            services.push_back("service_00" + std::to_string(count));
+        }
+
+        else if (count < 100) {
+            services.push_back("service_0" + std::to_string(count));
+        }
+
+        else {
+            services.push_back("service_" + std::to_string(count));
+        }
+    }
+
+    std::mt19937 random{};
+    std::uniform_int_distribution<int> services_dist(0, services.size()-1);
+    std::uniform_real_distribution<float> latency_dist(5.0, 50);
+    std::uniform_int_distribution<int> status_dist(1, 100);
+
     for (int count { 0 }; count < events; count++) {
-        output_file << "{\"ts\":" << count << ",\"service\":\"service_001\"" << ",\"status\":200,\"latency_ms\":10.0}\n";
+        int index = services_dist(random);
+        std::string service = services[index];
+
+        float latency = latency_dist(random);
+
+        int roll = status_dist(random);
+        int status = (roll <= 95) ? 200 : 500;
+
+    
+
+        output_file << "{\"timestamp\":" << timestamp
+            << ",\"service\":\"" << service << "\""
+            << ",\"status\":" << status
+            << ",\"latency_ms\":" << latency
+            << "}\n";
+
+        timestamp += 1;
     }
     
 
