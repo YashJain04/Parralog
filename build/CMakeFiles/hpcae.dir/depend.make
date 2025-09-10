623c4b1cdf5c9b698dcc653cd3b2f2b7b9982848
Yash Jain <yashjain200304@gmail.com>
2025-09-09 23:36:45 -0400

    base project setup

---
+++ build/CMakeFiles/hpcae.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hpcae.
+# This may be replaced when dependencies are built.
---
+++ src/hpcae/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "HPC Analytics Engine\n";
+    return 0;
+}
