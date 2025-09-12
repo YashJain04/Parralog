4d7e3de4697b76f62284dc53b07d23c1780b2e12
Yash Jain <yashjain200304@gmail.com>
2025-09-12 19:08:06 -0400

    adding memory mapping

--- src/hpcae/main.cpp
+++ src/hpcae/main.cpp
@@ -5,6 +5,12 @@
 #include <limits>
 #include <unordered_map>
 #include <iomanip>
+#include <fcntl.h>
+#include <unistd.h>
+#include "tools/p2_quantile_sketch.h"
+#include <sys/stat.h>
+#include <sys/mman.h>
+
 
 int main(int argc, char* argv[]) {
     std::ios::sync_with_stdio(false);
@@ -13,66 +19,89 @@ int main(int argc, char* argv[]) {
     std::cout << "HPC Analytics Engine\n";
     std::cout << "There are " << argc << " arguments\n";
 
-    std::ifstream input_file { argv[1] };
+    int file_descriptor = open(argv[1], O_RDONLY);
 
-    if (!input_file) {
-        std::cout << "Error processing input file\n";
+    if (file_descriptor == -1) {
+        std::cout << "Error reading file descriptor.";
+        return 1;
+    }
+    
+    struct stat metadata;
+
+    if (fstat(file_descriptor, &metadata) == -1) {
+        std::cout << "Error retrieving metadata with fstat.";
         return 1;
     }
 
-    std::string line {};
+    char *map;
 
+    map = static_cast<char*> (mmap (nullptr, metadata.st_size, PROT_READ, MAP_PRIVATE, file_descriptor, 0));
+
+    if (map == MAP_FAILED) {
+        std::cout << "Error memory mapping the file";
+        return 1;
+    }
+
+    size_t start = 0;
+    size_t end = 0;
     int total_events_processed = 0;
     int total_errors = 0;
     float total_latency = 0;
     float min_latency = std::numeric_limits<float>::infinity();
     float max_latency = 0;
     float average_latency = 0;
+    std::unordered_map<std::string, int> services_frequency;
 
     auto start_time = std::chrono::system_clock::now();
 
-    std::unordered_map<std::string, int> services_frequency;
+    P2QuantileSketch p50(0.5), p95(0.95), p99(0.99);
 
-    std::vector<float> latencies;
-    
-    while (std::getline(input_file, line)) {
-        // std::cout << line << "\n";
+    for (size_t i {0}; i < metadata.st_size; i++) {
 
-        std::size_t pos_ts = line.find("\"timestamp\":") + 12;
-        std::size_t pos_comma = line.find(",", pos_ts);
-        long long timestamp = std::stoll(line.substr(pos_ts, pos_comma - pos_ts));
+        if(map[i] == '\n') {
+            end = i;
+            std::string_view line(map + start, end - start);
 
-        std::size_t pos_service = line.find("\"service\":\"") + 10;
-        pos_comma = line.find(",", pos_service);
-        std::string service = line.substr(pos_service, pos_comma - pos_service);
+            size_t pos_status = line.find("\"status\":");
+            int status = 0;
+            if (pos_status != std::string_view::npos) {
+                size_t value_start = pos_status + 9; // skip over "status":
+                size_t value_end = line.find(',', value_start);
+                std::string_view status_str = line.substr(value_start, value_end - value_start);
+                status = std::stoi(std::string(status_str));
+                total_errors = (status == 200) ? total_errors : total_errors + 1;
+            }
 
-        services_frequency[service] += 1;
+            size_t pos_latency = line.find("\"latency_ms\":");
+            float latency = 0.0f;
+            if (pos_latency != std::string_view::npos) {
+                size_t value_start = pos_latency + 13;
+                size_t value_end = line.find(',', value_start);
+                std::string_view latency_str = line.substr(value_start, value_end - value_start);
+                latency = std::stof(std::string(latency_str));
 
-        std::size_t pos_status = line.find("\"status\":") + 9;
-        pos_comma = line.find(",", pos_status);
-        int status = std::stoi(line.substr(pos_status, pos_comma - pos_status));
+                min_latency = std::min(min_latency, latency);
+                max_latency = std::max(max_latency, latency);
+                total_latency += latency;
 
-        std::size_t pos_latency = line.find("\"latency_ms\":") + 13;
-        pos_comma = line.find(",", pos_latency);
-        float latency = static_cast<float>(std::stod(line.substr(pos_latency, pos_comma - pos_latency)));
+                p50.insert(latency);
+                p95.insert(latency);
+                p99.insert(latency);
 
-        latencies.push_back(latency);
+            }
 
-        // std::cout << "\n--CURRENT EVENT--\n";
-        // std::cout << "The timestamp is " << timestamp << "\n";
-        // std::cout << "The service is " << service << "\n";
+            size_t pos_service = line.find("\"service\":\"");
+            std::string service;
+            if (pos_service != std::string_view::npos) {
+                size_t value_start = pos_service + 11;
+                size_t value_end = line.find('"', value_start);
+                service = std::string(line.substr(value_start, value_end - value_start));
+                services_frequency[service] += 1;
+            }
+            total_events_processed += 1;
 
-        if (latency < min_latency) {
-            min_latency = latency;
+            start = i + 1;
         }
-
-        if (latency > max_latency) {
-            max_latency = latency;
-        }
-
-        total_latency += latency;
-        total_errors = (status == 200) ? total_errors : total_errors + 1;
-        total_events_processed += 1;
     }
 
     auto end_time = std::chrono::system_clock::now();
@@ -82,24 +111,16 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    std::sort(latencies.begin(), latencies.end());
-
-    size_t p50_index = static_cast<size_t>(0.50 * latencies.size());
-    float p50_latency = latencies[std::min(p50_index, latencies.size() - 1)];
-
-    size_t p95_index = static_cast<size_t>(0.95 * latencies.size());
-    float p95_latency = latencies[std::min(p95_index, latencies.size() - 1)];
-
-    size_t p99_index = static_cast<size_t>(0.99 * latencies.size());
-    float p99_latency = latencies[std::min(p99_index, latencies.size() - 1)];
-
+    float est_p50 = p50.getQuantile();
+    float est_p95 = p95.getQuantile();
+    float est_p99 = p99.getQuantile();
     average_latency = total_latency / total_events_processed;
 
     auto total_time = end_time - start_time;
     std::chrono::duration<double> seconds = total_time;
     double elapsed = seconds.count();
     double throughput = total_events_processed/elapsed;
-    double time_taken = total_events_processed / throughput;
+    double time_taken = elapsed;
     double error_percentage = (static_cast<double>(total_errors) / total_events_processed) * 100.0;
     
     std::vector<std::pair<std::string, int>> sorted_services(services_frequency.begin(), services_frequency.end());
@@ -123,9 +144,9 @@ int main(int argc, char* argv[]) {
     std::cout << "   Min        : " << std::fixed << std::setprecision(2) << min_latency << "\n";
     std::cout << "   Max        : " << max_latency << "\n";
     std::cout << "   Average    : " << average_latency << "\n";
-    std::cout << "   P50        : " << p50_latency << "\n";
-    std::cout << "   P95        : " << p95_latency << "\n";
-    std::cout << "   P99        : " << p99_latency << "\n\n";
+    std::cout << "   P50        : " << est_p50 << "\n";
+    std::cout << "   P95        : " << est_p95 << "\n";
+    std::cout << "   P99        : " << est_p99 << "\n\n";
 
     std::cout << "Performance:\n";
     std::cout << "   Throughput  : " << throughput << " events/sec\n";
@@ -139,5 +160,7 @@ int main(int argc, char* argv[]) {
 
     std::cout << "==============================\n\n";
 
+    munmap(map, metadata.st_size);
+    close(file_descriptor);
     return 0;
 }
