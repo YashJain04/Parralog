685aa307cad387a590ff7ffcacfbd48faa15acef
Yash Jain <yashjain200304@gmail.com>
2025-09-11 22:09:12 -0400

    added batching + buffering to loggen

--- tools/generate.cpp
+++ tools/generate.cpp
@@ -13,17 +13,20 @@ int main(int argc, char* argv[]) {
     std::ofstream output_file { argv[1] };
     int events { std::stoi(argv[2]) };
 
+    std::ios::sync_with_stdio(false);
+
     if (!output_file) {
         std::cerr << "Error processing output file\n";
         return 1;
     }
+
+    output_file.tie(nullptr);
 
     auto now = std::chrono::system_clock::now();
     auto ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
     long long timestamp = ms.time_since_epoch().count();
 
     std::vector <std::string> services;
-
 
     for (int count { 1 }; count < 101; count++) {
         if (count < 10) {
@@ -45,6 +48,10 @@ int main(int argc, char* argv[]) {
     std::uniform_real_distribution<float> latency_dist(5.0, 50);
     std::uniform_int_distribution<int> status_dist(1, 100);
 
+    const int BUFFER_SIZE = 1024;
+    std::string buffer[BUFFER_SIZE];
+    int buffer_index = 0;
+
     for (int count { 0 }; count < events; count++) {
         int index = services_dist(random);
         std::string service = services[index];
@@ -54,15 +61,30 @@ int main(int argc, char* argv[]) {
         int roll = status_dist(random);
         int status = (roll <= 95) ? 200 : 500;
 
-    
+        std::string record = "{\"timestamp\":" + std::to_string(timestamp)
+            + ",\"service\":\"" + service + "\""
+            + ",\"status\":" + std::to_string(status)
+            + ",\"latency_ms\":" + std::to_string(latency)
+            + "}\n";
 
-        output_file << "{\"timestamp\":" << timestamp
-            << ",\"service\":\"" << service << "\""
-            << ",\"status\":" << status
-            << ",\"latency_ms\":" << latency
-            << "}\n";
+        if (buffer_index >= BUFFER_SIZE) {    
+            for (int c { 0 }; c < BUFFER_SIZE; c++) {
+                output_file.write(buffer[c].c_str(), buffer[c].size());
+            }
+
+            buffer_index = 0;
+        }
+
+        buffer[buffer_index] = record;
+
+        if (count == events - 1) {
+            for (int c { 0 }; c < buffer_index + 1; c++) {
+                output_file.write(buffer[c].c_str(), buffer[c].size());
+            }
+        }
 
         timestamp += 1;
+        buffer_index += 1;
     }
 
     auto end_time = std::chrono::system_clock::now();
